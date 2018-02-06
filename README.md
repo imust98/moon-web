b42da89fbcf9afae0774607cb5a3deac244f621a
lhl <lihaoliang2006@126.com>
2018-02-06 10:21:48 +0800

    modify markdown

--- README.md
+++ README.md
@@ -1,4 +1,36 @@
-# vue-typescript-example
+# apm web端监控管理平台
 
+## 项目开发方式
 
-一个vue + typescript技术栈来开发项目的脚手架
+### 开发工具
+
+* 编辑器
+    * 推荐使用 vscode + vetur（对 typescript 支持较好）
+    * 编辑器安装 editorconfig
+* 浏览器
+    * 推荐安装扩展程序: Vue.js devtools
+
+### 安装依赖
+
+```shell
+npm install
+nei build -k 8c22091796974f8b247d452e70532fcb
+```
+
+### 前端本地开发步骤
+
+#### 运行
+
+```shell
+npm start
+```
+
+#### 测试
+
+```shell
+npm test
+```
+
+###开发规范
+1、自定义事件要以“on-”开头命名
+2、组件样式统一写在对应组件的style里面，并且样式中颜色部分统一用颜色变量表示，公共颜色变量在css/custom.mcss里，不需要自己引入，组件会自动引入。
--- public/src/views/App.vue
+++ public/src/views/App.vue
@@ -1,6 +1,26 @@
 <template>
   <div class="g-doc">
-    测试一下
+    <header class="g-head">
+      <div class="title"><strong>APM</strong></div>
+      <div class="userInfo"><strong></strong></div>
+    </header>
+    <div class="g-body">
+      <div class="g-side">
+        <div class="app"></div>
+        <nav>
+          <ul class="side">
+            <li>
+              <router-link class="tab" :to="{name:'userManage',query:{pid:pid}}" :class="{'tab-active':activeTab === 'userManage'}">
+                <span>JS Error分析</span>
+              </router-link>
+            </li>
+          </ul>
+        </nav>
+      </div>
+      <div class="g-main">
+        <router-view></router-view>
+      </div>
+    </div>
   </div>
 </template>
 <script lang="ts">
